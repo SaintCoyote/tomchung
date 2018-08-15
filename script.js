1ca091d17eab43decf871d09d9ce51229704b6ee
Tom Betthauser <tombetthauser@gmail.com>
2018-08-15 09:47:42 -0700

    new images / no text yet

--- script.js
+++ script.js
@@ -1,86 +1,253 @@
 // variables ––––––––––––––––––––––––––––––––––––––––––––––––––
 
-var a = [
+// test object
 
-	"End of Everything, Oil and Acrylic on Canvas, 144 x 216 in. 2018",
-	"End of Everything (Detail)",
-	"End of Everything (Detail)",
-	"Indras Net, Oil and Acrylic on Canvas, 62 x 64 in. 2017",
-	"Indras Net (Detail)",
-	"Many Headed Beast (Work in Progress)",
-	"The Trickster, Oil and Acrylic on Canvas, 134 x 68 in. 2017",
-	"Mother, Oil and Acrylic on Canvas, 52 x 65 in. 2016",
-	"Wedding Portrait for Mitchell, Acrylic on Canvas, 62 x 144 in. 2015",
-	"Jagermeister Knight, Acrylic and Oil on Canvas, 60 x 86 in. 2015",
-	"Everything, Oil and Acrylic on Canvas, 55 x 72 in. 2016",
-	"Whale Graffiti, Oil on Canvas, 32 x 40 in. 2016",
-	"Mother Nature Does Not love You, Oil and Acrylic on Fan, 65 x 50 in. 2016",
-	"Cheetoh Raven, Oil on Canvas, 10 x 8 in. 2016",
-	"Deer, Watercolor on Paper, 16 x 12 in. 2015",
-	"Jawbone Study, Gouache on paper, 18 x 24 in. 2015 ",
-	" Mouse Food, Acrylic on Canvas Panel, 48 x 24 in. 2016 Seal Mouth, Acrylic on Canvas Panel, 48 x 24 in. 2016 Talking Raven Acrylic on Canvas Panel, 48 x 24 in. 2016",
-	"Eyes, Acrylic on Canvas, 24 x 12 in. 2016",
-	"Teeth, Acrylic on Canvas, 24 x 12 in. 2016",
-	"Paws, Acrylic on Canvas, 24 x 12 in. 2016",
-	"Bones, Acrylic on Canvas, 24 x 12 in. 2016",
-	"Young Handsome Guy Seeks Fishing Buddy, Watercolor on Paper, 23 x 28 in. 2014",
-	"I Wish I Could Quit You, Acrylic on Canvas, 144 x 62 in. 2014",
-	"I Wish I Could Quit You (Detail)",
-	"I Wish I Could Quit You (Detail)",
-	"Bear Boobs, Acrylic on Canvas, 62 x 144 in. 2014",
-	"Bear Books (Detail)",
-	"Gay Tooth",
-	"From the Depths, Acrylic on Canvas, 144 x 62 in. 2014",
-	"From the Depths (In Progress Detail)",
-	"Highway 1, Watercolor on Paper, 23 x 28 in. 2014",
-	"Falling Eagles, Acrylic on Canvas, 96 x 62 in. 2015",
-	"I Really Caribou You, Acrylic on Canvas, 80 x 216 in. 2015",
-	" Aquarius Acrylic on Canvas, 144 x 64 in. 2015",
-	"Minotaur, Charcoal on paper, 23 x 29 in. 2015",
-	"Jones Gallery Install Shot",
-	"Jones Gallery Install Shot",
-	"Ocean Front Property (Detail Shot)",
-	"Ocean Front Property, Oil on Wood, 20.5 x 96 in. 2013",
-	"Ocean Front Property (Detail Shot)",
-	"Cheetoh Mandala",
-	"Jellyfish Robe (Relic from Performance)",
-	"Jellyfish Vials (Still from Film)",
-	"Jellyfish Robe (Detail Shot)",
-	"Jellyfish Robe (Detail Shot)",
-	"Jellyfish Robe (Detail Shot)",
-	"Remember What Happened to Captain Cook (Detail Shot)",
-	"Remember What Happened to Captain Cook, Acrylic Mural 11 x 25 ft. 2014",
-	"Remember What Happened to Captain Cook (Detail Shott)",
-	"So Spiritual, Scratched Leaf, Dimensions Variable 2014",
-	"So Spiritual, Scratched Leaf, Dimensions Variable 2014",
-	"Little Blonde Girl, Mixed Media, Artifact from Performance 2014",
-	"Little Blonde Girl (Detail Shot)",
-	"Still from Performance 2012",
-	"Cosmic Beast, Still from Performance 2012",
-	"Cosmic Beast, Still from Performance 2012",
-	"Creation Myth, Still from Performance 2012",
-	"Creation Myth, Still from Performance 2012",
-	"Birth of White Face Baby, Digital Photograph, 2012",
-	"Seed Beed Cheeto Bag, Installation, 2013",
-	"The Antidote, Digital Photograph, 2012",
-	"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
-	"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
-	"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
-	"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
-	"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
-	"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
-	"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
-	"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
-	"Air Between a Drum, Cowhide and Wood 2012",
-	"Air Between a Drum, Cowhide Wood 2012",
-	"Air Between a Drum, Cowhide Wood 2012",
-	"Shoot, Watercolor on Paper, 24 x 18 in. 2012",
-	"Pocahontas, Oil paint on Bear Hide, 38 x 22 in. 2012",
-	"G.I.'s Embrace, Oil on Canvas, 77 x 66 in. 2010",
-	"Buddha Lapdance, Oil on Canvas, 64 x 84 in. 2010",
-	"Facing the Hybrid, Oil on Canvas, 47 x 66 x in. 2010",
-	"Asian Explosion, Oil on Canvas, 96 x 80 in. 2010"
-]
+var artist = {
+	info: {
+		name: {
+			first:"Tom",
+			middle:"Pu",
+			last:"Chung",
+			displayMiddle: false
+		},
+		birth: {
+			year:"1989",
+			city:"Trenton",
+			state:"New Jersey",
+			country:"United States",
+			privacy: false
+		},
+		location: {
+			city:"Anchorage",
+			state:"Alaska",
+			country:"United States",
+			studio: {
+				street: "3211 Providence Drive",
+				city: "Anchorage",
+				state: "AK",
+				zip: "99508",
+				privacy: true
+			}
+		},
+		school:{
+			mfa: {
+				school:"Yale University",
+				year:"2013"
+			},
+			bfa: {
+				school:"San Francisco Art Institute",
+				year:"2013",
+			},
+			privacy: false
+		}
+	},
+	workAlt: [
+		{
+			title:"End of Everything",
+			medium:"Oil and Acrylic on Canvas",
+			height:"144",
+			width:"216",
+			increment:"inches",
+			year:"2018"
+		},{
+			title:"End of Everything (Detail)",
+			medium:"Oil and Acrylic on Canvas",
+			height:"144",
+			width:"216",
+			increment:"in.",
+			year:"2018"
+		},{
+			title:"End of Everything (Detail)",
+			medium:"Oil and Acrylic on Canvas",
+			height:"144",
+			width:"216",
+			increment:"in.",
+			year:"2018"
+		},{
+			title:"Indras Net",
+			medium:"Oil and Acrylic on Canvas",
+			height:"62",
+			width:"64",
+			increment:"in.",
+			year:"2017"
+		},{
+			title:"Indras Net (Detail)",
+			medium:"Oil and Acrylic on Canvas",
+			height:"62",
+			width:"64",
+			increment:"in.",
+			year:"2017"
+		}
+	],
+	work: [
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"End of Everything, Oil and Acrylic on Canvas, 144 x 216 in. 2018",
+		"End of Everything (Detail)",
+		"End of Everything (Detail)",
+		"Indras Net, Oil and Acrylic on Canvas, 62 x 64 in. 2017",
+		"Indras Net (Detail)",
+		"Many Headed Beast (Work in Progress)",
+		"The Trickster, Oil and Acrylic on Canvas, 134 x 68 in. 2017",
+		"Mother, Oil and Acrylic on Canvas, 52 x 65 in. 2016",
+		"Wedding Portrait for Mitchell, Acrylic on Canvas, 62 x 144 in. 2015",
+		"Jagermeister Knight, Acrylic and Oil on Canvas, 60 x 86 in. 2015",
+		"Everything, Oil and Acrylic on Canvas, 55 x 72 in. 2016",
+		"Whale Graffiti, Oil on Canvas, 32 x 40 in. 2016",
+		"Mother Nature Does Not love You, Oil and Acrylic on Fan, 65 x 50 in. 2016",
+		"Cheetoh Raven, Oil on Canvas, 10 x 8 in. 2016",
+		"Deer, Watercolor on Paper, 16 x 12 in. 2015",
+		"Jawbone Study, Gouache on paper, 18 x 24 in. 2015 ",
+		" Mouse Food, Acrylic on Canvas Panel, 48 x 24 in. 2016 Seal Mouth, Acrylic on Canvas Panel, 48 x 24 in. 2016 Talking Raven Acrylic on Canvas Panel, 48 x 24 in. 2016",
+		"Eyes, Acrylic on Canvas, 24 x 12 in. 2016",
+		"Teeth, Acrylic on Canvas, 24 x 12 in. 2016",
+		"Paws, Acrylic on Canvas, 24 x 12 in. 2016",
+		"Bones, Acrylic on Canvas, 24 x 12 in. 2016",
+		"Young Handsome Guy Seeks Fishing Buddy, Watercolor on Paper, 23 x 28 in. 2014",
+		"I Wish I Could Quit You, Acrylic on Canvas, 144 x 62 in. 2014",
+		"I Wish I Could Quit You (Detail)",
+		"I Wish I Could Quit You (Detail)",
+		"Bear Boobs, Acrylic on Canvas, 62 x 144 in. 2014",
+		"Bear Books (Detail)",
+		"Gay Tooth",
+		"From the Depths, Acrylic on Canvas, 144 x 62 in. 2014",
+		"From the Depths (In Progress Detail)",
+		"Highway 1, Watercolor on Paper, 23 x 28 in. 2014",
+		"Falling Eagles, Acrylic on Canvas, 96 x 62 in. 2015",
+		"I Really Caribou You, Acrylic on Canvas, 80 x 216 in. 2015",
+		" Aquarius Acrylic on Canvas, 144 x 64 in. 2015",
+		"Minotaur, Charcoal on paper, 23 x 29 in. 2015",
+		"Jones Gallery Install Shot",
+		"Jones Gallery Install Shot",
+		"Ocean Front Property (Detail Shot)",
+		"Ocean Front Property, Oil on Wood, 20.5 x 96 in. 2013",
+		"Ocean Front Property (Detail Shot)",
+		"Cheetoh Mandala",
+		"Jellyfish Robe (Relic from Performance)",
+		"Jellyfish Vials (Still from Film)",
+		"Jellyfish Robe (Detail Shot)",
+		"Jellyfish Robe (Detail Shot)",
+		"Jellyfish Robe (Detail Shot)",
+		"Remember What Happened to Captain Cook (Detail Shot)",
+		"Remember What Happened to Captain Cook, Acrylic Mural 11 x 25 ft. 2014",
+		"Remember What Happened to Captain Cook (Detail Shott)",
+		"So Spiritual, Scratched Leaf, Dimensions Variable 2014",
+		"So Spiritual, Scratched Leaf, Dimensions Variable 2014",
+		"Little Blonde Girl, Mixed Media, Artifact from Performance 2014",
+		"Little Blonde Girl (Detail Shot)",
+		"Still from Performance 2012",
+		"Cosmic Beast, Still from Performance 2012",
+		"Cosmic Beast, Still from Performance 2012",
+		"Creation Myth, Still from Performance 2012",
+		"Creation Myth, Still from Performance 2012",
+		"Birth of White Face Baby, Digital Photograph, 2012",
+		"Seed Beed Cheeto Bag, Installation, 2013",
+		"The Antidote, Digital Photograph, 2012",
+		"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
+		"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
+		"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
+		"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
+		"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
+		"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
+		"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
+		"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
+		"Air Between a Drum, Cowhide and Wood 2012",
+		"Air Between a Drum, Cowhide Wood 2012",
+		"Air Between a Drum, Cowhide Wood 2012",
+		"Shoot, Watercolor on Paper, 24 x 18 in. 2012",
+		"Pocahontas, Oil paint on Bear Hide, 38 x 22 in. 2012",
+		"G.I.'s Embrace, Oil on Canvas, 77 x 66 in. 2010",
+		"Buddha Lapdance, Oil on Canvas, 64 x 84 in. 2010",
+		"Facing the Hybrid, Oil on Canvas, 47 x 66 x in. 2010",
+		"Asian Explosion, Oil on Canvas, 96 x 80 in. 2010"
+	]
+}
+
+// var a = [
+
+// 	"End of Everything, Oil and Acrylic on Canvas, 144 x 216 in. 2018",
+// 	"End of Everything (Detail)",
+// 	"End of Everything (Detail)",
+// 	"Indras Net, Oil and Acrylic on Canvas, 62 x 64 in. 2017",
+// 	"Indras Net (Detail)",
+// 	"Many Headed Beast (Work in Progress)",
+// 	"The Trickster, Oil and Acrylic on Canvas, 134 x 68 in. 2017",
+// 	"Mother, Oil and Acrylic on Canvas, 52 x 65 in. 2016",
+// 	"Wedding Portrait for Mitchell, Acrylic on Canvas, 62 x 144 in. 2015",
+// 	"Jagermeister Knight, Acrylic and Oil on Canvas, 60 x 86 in. 2015",
+// 	"Everything, Oil and Acrylic on Canvas, 55 x 72 in. 2016",
+// 	"Whale Graffiti, Oil on Canvas, 32 x 40 in. 2016",
+// 	"Mother Nature Does Not love You, Oil and Acrylic on Fan, 65 x 50 in. 2016",
+// 	"Cheetoh Raven, Oil on Canvas, 10 x 8 in. 2016",
+// 	"Deer, Watercolor on Paper, 16 x 12 in. 2015",
+// 	"Jawbone Study, Gouache on paper, 18 x 24 in. 2015 ",
+// 	" Mouse Food, Acrylic on Canvas Panel, 48 x 24 in. 2016 Seal Mouth, Acrylic on Canvas Panel, 48 x 24 in. 2016 Talking Raven Acrylic on Canvas Panel, 48 x 24 in. 2016",
+// 	"Eyes, Acrylic on Canvas, 24 x 12 in. 2016",
+// 	"Teeth, Acrylic on Canvas, 24 x 12 in. 2016",
+// 	"Paws, Acrylic on Canvas, 24 x 12 in. 2016",
+// 	"Bones, Acrylic on Canvas, 24 x 12 in. 2016",
+// 	"Young Handsome Guy Seeks Fishing Buddy, Watercolor on Paper, 23 x 28 in. 2014",
+// 	"I Wish I Could Quit You, Acrylic on Canvas, 144 x 62 in. 2014",
+// 	"I Wish I Could Quit You (Detail)",
+// 	"I Wish I Could Quit You (Detail)",
+// 	"Bear Boobs, Acrylic on Canvas, 62 x 144 in. 2014",
+// 	"Bear Books (Detail)",
+// 	"Gay Tooth",
+// 	"From the Depths, Acrylic on Canvas, 144 x 62 in. 2014",
+// 	"From the Depths (In Progress Detail)",
+// 	"Highway 1, Watercolor on Paper, 23 x 28 in. 2014",
+// 	"Falling Eagles, Acrylic on Canvas, 96 x 62 in. 2015",
+// 	"I Really Caribou You, Acrylic on Canvas, 80 x 216 in. 2015",
+// 	" Aquarius Acrylic on Canvas, 144 x 64 in. 2015",
+// 	"Minotaur, Charcoal on paper, 23 x 29 in. 2015",
+// 	"Jones Gallery Install Shot",
+// 	"Jones Gallery Install Shot",
+// 	"Ocean Front Property (Detail Shot)",
+// 	"Ocean Front Property, Oil on Wood, 20.5 x 96 in. 2013",
+// 	"Ocean Front Property (Detail Shot)",
+// 	"Cheetoh Mandala",
+// 	"Jellyfish Robe (Relic from Performance)",
+// 	"Jellyfish Vials (Still from Film)",
+// 	"Jellyfish Robe (Detail Shot)",
+// 	"Jellyfish Robe (Detail Shot)",
+// 	"Jellyfish Robe (Detail Shot)",
+// 	"Remember What Happened to Captain Cook (Detail Shot)",
+// 	"Remember What Happened to Captain Cook, Acrylic Mural 11 x 25 ft. 2014",
+// 	"Remember What Happened to Captain Cook (Detail Shott)",
+// 	"So Spiritual, Scratched Leaf, Dimensions Variable 2014",
+// 	"So Spiritual, Scratched Leaf, Dimensions Variable 2014",
+// 	"Little Blonde Girl, Mixed Media, Artifact from Performance 2014",
+// 	"Little Blonde Girl (Detail Shot)",
+// 	"Still from Performance 2012",
+// 	"Cosmic Beast, Still from Performance 2012",
+// 	"Cosmic Beast, Still from Performance 2012",
+// 	"Creation Myth, Still from Performance 2012",
+// 	"Creation Myth, Still from Performance 2012",
+// 	"Birth of White Face Baby, Digital Photograph, 2012",
+// 	"Seed Beed Cheeto Bag, Installation, 2013",
+// 	"The Antidote, Digital Photograph, 2012",
+// 	"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
+// 	"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
+// 	"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
+// 	"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
+// 	"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
+// 	"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
+// 	"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
+// 	"Yale MFA 2013 Thesis Exhibition, Installation, 2013",
+// 	"Air Between a Drum, Cowhide and Wood 2012",
+// 	"Air Between a Drum, Cowhide Wood 2012",
+// 	"Air Between a Drum, Cowhide Wood 2012",
+// 	"Shoot, Watercolor on Paper, 24 x 18 in. 2012",
+// 	"Pocahontas, Oil paint on Bear Hide, 38 x 22 in. 2012",
+// 	"G.I.'s Embrace, Oil on Canvas, 77 x 66 in. 2010",
+// 	"Buddha Lapdance, Oil on Canvas, 64 x 84 in. 2010",
+// 	"Facing the Hybrid, Oil on Canvas, 47 x 66 x in. 2010",
+// 	"Asian Explosion, Oil on Canvas, 96 x 80 in. 2010"
+// ]
 
 
 // script ––––––––––––––––––––––––––––––––––––––––––––––––––
@@ -88,44 +255,11 @@ var a = [
 
 loadNavbar();
 
-// loadHeader();
-
 loadImages();
 
 
 // functions ––––––––––––––––––––––––––––––––––––––––––––––––––
 
-
-
-// function loadHeader() {
-// 	$("body").append(
-// 		+ '<div class="col-xs-0 col-sm-1 col-md-2 col-lg-3"></div>'
-// 		+ '<div class="col-xs-12 col-sm-10 col-md-8 col-lg-6">'
-// 		+ 		'<div class="t-nav1-container hidden-xs">'
-// 		+ 			'<div class="row">'
-// 		+ 				'<div class="col-xs-12">'
-// 		+ 					'<a href="index.html"><h1 class="t-center" id="t-title-1">Tom Chung</h1></a>'
-// 		+ 				'</div>'
-// 		+ 			'</div>'
-// 		+ 			'<div class="row">'
-// 		+ 				'<div class="col-xs-12">'
-// 		+ 					'<div class="t-center" id="t-nav-links">'
-// 		+ 						  '<a href="index.html"><span class="t-left-cap">Home</a>'
-// 		+ 						  '<span class="t-slash">/</span>'
-// 		+ 						  '<a href="images.html">Images</a>'
-// 		+ 						  '<span class="t-slash">/</span>'
-// 		+ 						  '<a href="info.html">Information</a>'
-// 		+ 						  '<span class="t-slash">/</span>'
-// 		+ 						  '<a href="links.html">Links</a>'
-// 		+ 					'</div>'
-// 		+ 				'</div>'
-// 		+ 			'</div>'
-// 		+ 		'</div>'
-// 		+		'<div id="container"></div>'
-//  	+		'<br><br><br><br><br>'
-// 		+	'</div>	'
-// 	)
-// }
 
 function loadNavbar() {
 	  $("body").prepend(
@@ -155,7 +289,7 @@ function loadNavbar() {
 }
 
 function loadImages() {
-	for(i = a.length; i > 0 ; i--){
+	for(i = artist.work.length; i > 0 ; i--){
 		  $("#container").append(
 		    "<a target='new' href='images/full/"
 		    + (i)
@@ -163,7 +297,7 @@ function loadImages() {
 		    + (i)
 		    + ".jpg'></a>"
  			+ "<p class='t-title-etc'>"
- 			+ a[a.length-(i)]
+ 			+ artist.work[artist.work.length-(i)]
  			+ "</p>"
 		  ) 
 	}
